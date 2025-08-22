654e516fca5eb68fec54d67512355bee01ae1d03
Ashish Chandra Acharjee <acharjeeashish2@gmail.com>
2025-08-22 05:52:45 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/AI-Doctor-Assistance',
+  assetPrefix: '/AI-Doctor-Assistance/',
+};
+
+module.exports = nextConfig;
